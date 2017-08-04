815f6937e51999cc3ae53831e182621ff2f33084
valdadyne <rodgers.gitau@gmail.com>
2017-08-04 18:34:53 +0300

    InstalledVuetify and Created a Responsive Navigation

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-title-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-title-action>
+          <v-list-tile-content>View Events</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon 
+        @click.native.stop="sideNav = !sideNav"
+        class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>Volunteer Kenya</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left>supervisor_account</v-icon>
+          View Events
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+    </main>   
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        sideNav: false
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
